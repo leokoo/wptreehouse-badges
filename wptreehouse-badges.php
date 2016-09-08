9265d6a9ed4d03c760725f2302d443c9be88d6b4
Leo Koo <leokoo@gmail.com>
2016-09-09 04:11:43 +0800

    first commit

---
+++ wptreehouse-badges.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ *	Plugin Name: Official Treehouse Badges Plugin
+ *	Plugin URI: http://wptreehouse.com/wptreehouse-badges-plugin/
+ *	Description: Provides both widgets and shortcodes to help you display your Treehouse profile badges on your website.  The official Treehouse badges plugin.
+ *	Version: 1.0
+ *	Author: Zac Gordon
+ *	Author URI: http://wp.zacgordon.com
+ *	License: GPL2
+ *
+*/
+
+/*
+ *	Assign global variables
+ *
+*/
+
+$plugin_url = WP_PLUGIN_URL . '/wptreehouse-badges';
+
+/*
+ *	Add a link to our plugin in the admin menu
+ *	under 'Settings > Treehouse Badges'
+ *
+*/
+
+function wptreehouse_badges_menu() {
+
+	/*
+	 * 	Use the add_options_page function
+	 * 	add_options_page( $page_title, $menu_title, $capability, $menu-slug, $function ) 
+	 *
+	*/
+
+	add_options_page(
+		'Official Treehouse Badges Plugin',
+		'Treehouse Badges',
+		'manage_options',
+		'wptreehouse-badges',
+		'wptreehouse_badges_options_page'
+	);
+
+}
+add_action( 'admin_menu', 'wptreehouse_badges_menu' );
+
+
+function wptreehouse_badges_options_page() {
+
+	if( !current_user_can( 'manage_options' ) ) {
+
+		wp_die( 'You do not have suggicient permissions to access this page.' );
+
+	}
+
+	global $plugin_url;
+
+	require( 'inc/options-page-wrapper.php' );
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
